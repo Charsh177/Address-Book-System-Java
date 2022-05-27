0694ef95728ac234b897911826b28d874edb5529
Charsh177 <harshchougule177@gmail.com>
2022-05-27 11:56:34 +0530

    Welcome to address book program

---
+++ src/com/addressbook/za/AddressBookProgram.java
@@ -0,0 +1,9 @@
+package com.addressbook.za;
+
+public class AddressBookProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program");
+    }
+}
